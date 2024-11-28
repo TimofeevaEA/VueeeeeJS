0a1ad82b21a9edf43b7f9f16b422a6b51d2671e5
Тимофеева Эвелина Артёмовна <TimofeevaEA@DOM201.LOCAL>
2024-11-28 10:28:45 +0700

    котики

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <h1>Привееееет кд чд пака</h1>
+  <img src="https://masterpiecer-images.s3.yandex.net/a73b12916cf411ee8e7f3a7ca4cc1bdc:upscaled" alt="">
   <layout-default>
     <router-view></router-view>
   </layout-default>
--- src/components/UsersPage/UsersTable.vue
+++ src/components/UsersPage/UsersTable.vue
@@ -5,59 +5,75 @@
       <div class="users-table__cell">Имя</div>
       <div class="users-table__cell">Почта</div>
       <div class="users-table__cell">Дата создания</div>
+      <div class="users-table__cell">Аватар</div>
     </div>
     <div v-for="user in users" :key="user.id" class="users-table__row">
       <div class="users-table__cell">{{ user.id }}</div>
       <div class="users-table__cell">{{ user.name }}</div>
       <div class="users-table__cell">{{ user.email }}</div>
       <div class="users-table__cell">{{ user.created }}</div>
+      <div class="users-table__cell">
+        <img :src="user.avatar" alt="Аватар" class="user-avatar" />
+      </div>
     </div>
   </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, computed } from 'vue'
-  
-  type UserType = {
-    id: number
-    name: string
-    email: string
-    created: string | Date
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+type UserType = {
+  id: number
+  name: string
+  email: string
+  created: string | Date
+  avatar: string
+}
+
+const currentDate = new Date()
+
+const createdMockDate = computed(() => {
+  return `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()}`
+})
+
+const users = ref<UserType[]>([
+  {
+    id: 0,
+    name: 'Ann',
+    email: 'alex@alex.alex',
+    created: createdMockDate.value,
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy7X3UeIJ4hf3RVDDjLzfwuXuvW0n-ZjsSFg&s'
+  },
+  {
+    id: 1,
+    name: 'Alex',
+    email: 'ann@ann.ann',
+    created: createdMockDate.value,
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkBlmgTNf0MMCV705z4_e4bXC9DMp4l4JYoA&s'
+  },
+  {
+    id: 2,
+    name: 'Art',
+    email: 'art@art.art',
+    created: createdMockDate.value,
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5qFjphJ-i7TLsMm7ZBOwAvnAec_KpPv3Sgg&s'
+  },
+  {
+    id: 3,
+    name: 'you',
+    email: 'you@you.you',
+    created: createdMockDate.value,
+    avatar: 'https://cool.klev.club/uploads/posts/2024-05/cool-klev-club-gsch-p-prikolnie-kartinki-milie-kotiki-na-avu-2.jpg'
+  },
+  {
+    id: 4,
+    name: 'artemka',
+    email: 'artyomkaprivprivetik',
+    created: createdMockDate.value,
+    avatar: 'https://vet-centre.by/wp-content/uploads/2016/11/kot-eti-udivitelnye-kotiki.jpg'
   }
-  
-  const currentDate = new Date()
-  
-  const createdMockDate = computed(() => {
-    return `${currentDate.getDate()}.${currentDate.getMonth()}.${currentDate.getFullYear()}`
-  })
-  
-  const users = ref<UserType[]>([
-    {
-      id: 0,
-      name: 'Ann',
-      email: 'alex@alex.alex',
-      created: createdMockDate.value,
-    },
-    {
-      id: 1,
-      name: 'Alex',
-      email: 'ann@ann.ann',
-      created: createdMockDate.value,
-    },
-    {
-      id: 2,
-      name: 'Art',
-      email: 'art@art.art',
-      created: createdMockDate.value,
-    },
-    {
-      id: 3,
-      name: 'you',
-      email: 'you@you.you',
-      created: createdMockDate.value,
-    }
-  ])
-  </script>
+])
+</script>
   
   <style scoped>
   .users-table {
@@ -67,14 +83,19 @@
   
   .users-table__row {
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(5, 1fr);
   }
   
   .users-table__cell {
     min-width: 120px;
-    background-color: #888888;
-    border: 1px solid cornsilk;
+    background-color: #d8d3d3;
+    border: 3px solid rgb(41, 31, 39);
     text-align: center;
-    padding: 4px;
+    padding: 80px;
   }
+  .user-avatar {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+}
   </style>
